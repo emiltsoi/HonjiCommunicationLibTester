39413950f93e25aaf02ff05b0a0f189a1be8ec57
Emil Tsoi <tsoiyanhung@gmail.com>
2018-11-04 17:22:45 +0800

    Add library support for MFC reading; Add gas control page to tester.

--- DisplayUpdateHelper.cs
+++ DisplayUpdateHelper.cs
@@ -23,5 +23,12 @@ namespace SiemensCommunicatinLibTester
                 stateText = "Unknown";
             textbox.Text = stateText;
         }
+
+        public static void UpdateMFCState(IMassFlowController mfc, TextBox reading, TextBox valveOpen, TextBox mfcOn)
+        {
+            reading.Text = mfc.GetFlowReading().ToString() + "sccm";
+            valveOpen.Text = mfc.IsShutoffValveOpen() ? "Open" : "Close";
+            mfcOn.Text = mfc.IsMFCOn() ? "On" : "Off";
+        }
     }
 }
