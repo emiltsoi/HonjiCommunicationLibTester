230441fed29dd5e2038a8186060f8b9d34b5af96
Emil Tsoi <tsoiyanhung@gmail.com>
2018-11-04 14:54:49 +0800

    Add utility to library; created interface for displaying/controlling it.

---
+++ DisplayUpdateHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using HonjiS1200CommLib;
+
+namespace SiemensCommunicatinLibTester
+{
+    class DisplayUpdateHelper
+    {
+        public static void UpdateValveState(IValve valve, TextBox textbox)
+        {
+            string stateText;
+            if (valve.IsOpen())
+                stateText = "Open";
+            else if (valve.IsClosed())
+                stateText = "Closed";
+            else if (valve.IsInError())
+                stateText = "Error";
+            else
+                stateText = "Unknown";
+            textbox.Text = stateText;
+        }
+    }
+}
